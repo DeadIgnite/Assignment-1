814aee9b63b12835c813db753646548b848bb7d1
Cody Zellmer-Johnson <czellmer12@gmail.com>
2023-11-02 22:05:37 -0500

    Release 1: Allows for the adding of and the viewing of pets.

---
+++ src/main/java/Pet.java
@@ -0,0 +1,16 @@
+/*
+    Author: Cody Zellmer-Johnson
+    Class: CSC 422
+    Assignment: Version Control Pet Database
+    Release: 1
+ */
+public class Pet {
+    private String name;
+    private int age;
+    public Pet(String name, int age) {this.name = name; this.age = age;}
+    // getters and setters
+    public String getName() {return this.name;}
+    public int getAge() {return this.age;}
+    public void setName(String name) {this.name = name;}
+    public void setAge(int age) {this.age = age;}
+}
---
+++ src/main/java/PetDatabase.java
@@ -0,0 +1,95 @@
+/*
+    Author: Cody Zellmer-Johnson
+    Class: CSC 422
+    Assignment: Version Control Pet Database
+    Release: 1
+ */
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PetDatabase {
+    // list and scanner to be used throughout the application
+    private static final ArrayList<Pet> petList = new ArrayList<>();
+    private static final Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Pet Database Program.");
+        // calls getUserDecision function and uses switch statement to call correct function
+        do {
+            switch (getUserDecision()) {
+                case "1" -> showAllPets();
+                case "2" -> addPet();
+                case "3" -> {
+                    System.out.println("Thank you for using the Pet Database! Goodbye!");
+                    System.exit(0);
+                }
+            }
+        } while (true);
+    }
+
+    private static String getUserDecision() {
+        // prints table of choices
+        System.out.println("What would you like to do?");
+        System.out.println("\t1) View all pets");
+        System.out.println("\t2) Add more pets");
+        System.out.println("\t3) Exit program");
+        System.out.print("Your choice: ");
+        // returns the users choice
+        return s.next();
+    }
+
+    private static void printDashes() {
+        // prints + at the beginning and end, dashes in the middle
+        for (int i = 0; i <= 25; i++) {
+            if (i == 0 || i == 25) {
+                System.out.print("+");
+            } else {
+                System.out.print("-");
+            }
+        }
+        System.out.print("\n");
+    }
+
+    // prints table header when called
+    private static void printTableHeader() {
+        printDashes();
+        System.out.printf("| %3s | %-10s| %4s |\n", "ID", "Name", "Age");
+        printDashes();
+    }
+
+    // prints table footer when called
+    private static void printTableFooter(int rowCount) {
+        printDashes();
+        System.out.println(rowCount + " rows in set.");
+    }
+
+    // prints a table row using information from passed in Pet object when called
+    private static void printTableRow(Pet pet) {
+        System.out.printf("| %-3d | %-10s| %4d |\n", petList.indexOf(pet), pet.getName(), pet.getAge());
+    }
+
+    private static void showAllPets() {
+        printTableHeader();
+        // for each pet in the list, print a row
+        for (Pet pet : petList) {
+            printTableRow(pet);
+        }
+        printTableFooter(petList.size());
+    }
+
+    private static void addPet() {
+        do {
+            // gets name and age
+            System.out.print("Enter the pet's name (type 'done' to exit): ");
+            String name = s.next();
+            // if the user types done break the loop
+            if (name.equalsIgnoreCase("done")) {
+                break;
+            }
+            System.out.print("Enter the pet's age: ");
+            int age = s.nextInt();
+            // creates new pet object and adds it to list
+            petList.add(new Pet(name, age));
+        } while (true);
+    }
+}
